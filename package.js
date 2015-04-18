bba19250bbf7e2ff0d03fc887bac6db4aaf09e5a
Julian Ćwirko <julian.cwirko@gmail.com>
2015-04-18 16:28:39 +0200

    git url

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     'summary': 'Genie effect for s-alert - simple and fancy notifications / alerts for Meteor.',
     'version': '1.0.0',
-    'git': 'https://github.com/juliancwirko/meteor-s-alert.git',
+    'git': 'https://github.com/juliancwirko/meteor-s-alert-genie.git',
     'name': 'juliancwirko:s-alert-genie'
 });
 
